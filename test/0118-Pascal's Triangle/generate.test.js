5b29e49da70b4eb0151b1629282143e01d2b9b34
YuTengjing <ytj2713151713@gmail.com>
2020-01-17 17:53:03 +0800

    :lipstick: style: remove the useless decorator # before test desc

--- test/0118-Pascal's Triangle/generate.test.js
+++ test/0118-Pascal's Triangle/generate.test.js
@@ -3,13 +3,13 @@ const assert = require('assert');
 const generate = require(`../../src/0118-Pascal's Triangle/generate`);
 
 describe(`#0118-Pascal's Triangle`, () => {
-    it('#should numRows equals 0 return [] ', () => {
+    it('should numRows equals 0 return [] ', () => {
         assert.deepEqual(generate(0), []);
     });
-    it('#should numRows equals 1 return [[1]]', () => {
+    it('should numRows equals 1 return [[1]]', () => {
         assert.deepEqual(generate(1), [[1]]);
     });
-    it('#when numRows equals 5', () => {
+    it('when numRows equals 5', () => {
         const result = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]];
         assert.deepEqual(generate(5), result);
     });

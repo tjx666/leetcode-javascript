ba55bbda71758e701bf71831396c17f8657d611b
YuTengjing <ytj2713151713@gmail.com>
2020-04-04 20:10:56 +0800

    :sparkles: feat; 喝醉酒也要刷题

---
+++ src/0167-Two Sum II - Input array is sorted/twoSum.js
@@ -0,0 +1,17 @@
+/**
+ * 有序数组找两数之和等于目标数组的小标，下标从 1 开始
+ * 思路：有序的情况下，使用首尾双指针
+ *
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(numbers, target) {
+    let head = 0;
+    let tail = numbers.length - 1;
+
+    while (head !== tail) {
+        if (numbers[head] + numbers[tail] === target) return [head + 1, tail + 1];
+        numbers[head] + numbers[tail] > target ? --tail : ++head;
+    }
+}

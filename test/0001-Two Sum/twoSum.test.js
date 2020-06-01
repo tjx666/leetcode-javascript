b3889d43ed33ef021afe18df7c44d1301530cf6a
YuTengjing <ytj2713151713@gmail.com>
2020-06-01 15:53:37 +0800

    :white_check_mark: fix tests

--- test/0001-Two Sum/twoSum.test.js
+++ test/0001-Two Sum/twoSum.test.js
@@ -8,9 +8,9 @@ describe('#0001-Two Sum', () => {
     [twoSum1, twoSum2, twoSum3].forEach((twoSum, index) => {
         describe(`#twoSum${index + 1}`, () => {
             const testArray = [2, 1, 3, 5];
-            it(`should twoSum(testArray, 7) return '0,3' or '3,0'`, () => {
-                const result = twoSum(testArray, 7).toString();
-                assert.ok(result === '0,3' || result === '3,0');
+            it(`should twoSum(testArray, 4) return '1,2' or '2,1'`, () => {
+                const result = twoSum(testArray, 4).toString();
+                assert.ok(result === '1,2' || result === '2,1');
             });
         });
     });

6f0b4e429583684c9ae5065633b576bab284b86a
YuTengjing <ytj2713151713@gmail.com>
2020-06-12 22:46:07 +0800

    :white_check_mark: 提高测试覆盖率

--- test/0001-Two Sum/twoSum.test.js
+++ test/0001-Two Sum/twoSum.test.js
@@ -7,10 +7,10 @@ const twoSum3 = require('../../src/0001-Two Sum/twoSum3');
 describe('#0001-Two Sum', () => {
     [twoSum1, twoSum2, twoSum3].forEach((twoSum, index) => {
         describe(`#twoSum${index + 1}`, () => {
-            const testArray = [2, 1, 3, 5];
-            it(`should twoSum(testArray, 4) return '1,2' or '2,1'`, () => {
-                const result = twoSum(testArray, 4).toString();
-                assert.ok(result === '1,2' || result === '2,1');
+            const testArray = [2, 1, 4, 7];
+            it(`should twoSum(testArray, 6) return '0,2' or '2,0'`, () => {
+                const result = twoSum(testArray, 6).toString();
+                assert.ok(result === '0,2' || result === '2,0');
             });
         });
     });

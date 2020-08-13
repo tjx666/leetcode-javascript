5b2a6ed34646c0b7f3ad4f321a89869aa8d7c870
YuTengjing <ytj2713151713@gmail.com>
2020-08-14 01:02:40 +0800

    优化 43-大数相乘

--- src/0043-Multiply Strings/solution.js
+++ src/0043-Multiply Strings/solution.js
@@ -1,8 +1,5 @@
 /**
- * 题述：大数相乘
- * 思路：模拟手算过程
- * 时间复杂度：m * n
- * 空间复杂度：m + n
+ * 题述： 大数相乘
  * @param {string} num1
  * @param {string} num2
  * @return {string}
@@ -10,9 +7,18 @@
 function multiply(num1, num2) {
     if (num1 === '0' || num2 === '0') return '0';
 
+    // 当 num1 0 结尾时例如 1010000000，将 后面的 0 移到 num2 后面
+    const groups = num1.match(/^(\d+?)((?<=[^0])0+$)/);
+    if (groups) {
+        const [_, noneZeroPrefix, zeroPostfix] = groups;
+        num1 = noneZeroPrefix;
+        num2 = `${num2}${zeroPostfix}`;
+    }
+
     let sum = '0';
     for (let j = num2.length - 1; j >= 0; j--) {
         const bit2 = num2[j];
+        if (bit2 === 0) continue;
         let bitSum = '';
         let carry = 0;
         for (let i = num1.length - 1; i >= 0; i--) {
@@ -27,7 +33,7 @@ function multiply(num1, num2) {
         bitSum = `${bitSum}${new Array(num2.length - 1 - j).fill('0').join('')}`;
         sum = addStrings(sum, bitSum);
     }
-    return sum.replace(/^0+/, '');
+    return sum;
 }
 
 /**

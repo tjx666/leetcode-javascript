d409ba6f0d7c98dc5e823821c76c4cfac6bd39af
YuTengjing <ytj2713151713@gmail.com>
2020-09-10 00:05:37 +0800

    60-第 k 个排列

---
+++ src/0060-Permutation Sequence/solution1.js
@@ -0,0 +1,35 @@
+/**
+ * 题述：第k个排列
+ * 思路：回溯
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+function getPermutation(n, k) {
+    const candidates = Array.from({ length: n }).map((_, index) => index + 1);
+    const visited = Array.from({ length: n }).fill(false);
+    const path = [];
+    let result;
+
+    function backTrace() {
+        if (path.length === n) {
+            k--;
+            if (k === 0) {
+                result = [...path];
+            }
+            return;
+        }
+
+        for (let i = 0; i < n && k !== 0; i++) {
+            if (!visited[i]) {
+                path.push(candidates[i]);
+                visited[i] = true;
+                backTrace();
+                path.pop();
+                visited[i] = false;
+            }
+        }
+    }
+    backTrace();
+    return result.join('');
+}
